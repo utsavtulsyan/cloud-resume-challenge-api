d849450cbe301ec9628b571834dbce3430bfa688
Utsav Tulsyan <utsavtulsyan44@gmail.com>
2023-12-21 19:20:07 +0530

    updated fetch query to use sdk instead of binding for error trace

--- GetVisitorCount/__init__.py
+++ GetVisitorCount/__init__.py
@@ -4,15 +4,22 @@ import azure.functions as func
 from azure.data.tables import TableClient, UpdateMode
 import os
 
-def main(req: func.HttpRequest, userCount) -> func.HttpResponse:
+def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-    userCountJson = json.loads(userCount)
-    userCountJson[0]['count'] += 1 
-    with TableClient.from_connection_string(
-        conn_str=os.environ["AzureWebJobsStorage"], table_name="resume"
-   ) as table_client:
-        table_client.update_entity(mode=UpdateMode.REPLACE, entity=userCountJson[0])
-        logging.info('count update successful!')
+    
+    try:
+        with TableClient.from_connection_string(
+            conn_str=os.environ["AzureWebJobsStorage"], table_name="resume"
+    ) as table_client:
+            
+            userCountJson = table_client.get_entity("", "")
+            userCountJson['count'] += 1 
+            table_client.update_entity(mode=UpdateMode.REPLACE, entity=userCountJson)
+            logging.info('count update successful!')
+            return func.HttpResponse(str(userCountJson['count']))
+    except Exception as e:
+        logging.error('Failed with error: %s', e)
+        return func.HttpResponse('internal error', status_code=500)
 
-    return func.HttpResponse(str(userCountJson[0]['count']))
+
 
